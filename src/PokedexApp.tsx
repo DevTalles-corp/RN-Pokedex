9afc797f1bde661db27981d8dfeb8d1e0b9277ac
Fernando Herrera <fernando.herrera85@gmail.com>
2024-01-30 14:04:08 -0500

    Add react-native-paper library and update dependencies

--- src/PokedexApp.tsx
+++ src/PokedexApp.tsx
@@ -2,11 +2,14 @@ import 'react-native-gesture-handler';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {StackNavigator} from './presentation/navigator/StackNavigator';
+import {PaperProvider} from 'react-native-paper';
 
 export const PokedexApp = () => {
   return (
-    <NavigationContainer>
-      <StackNavigator />
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,10 +1,16 @@
-import { Text, View } from 'react-native';
+import {Text, View} from 'react-native';
+import { Button } from 'react-native-paper';
 
 export const HomeScreen = () => {
   return (
     <View>
       <Text>HomeScreen</Text>
-      
+
+      <Button
+        mode="contained"
+        onPress={() => console.log('Pressed')}>
+        Press me
+      </Button>
     </View>
-  )
-}
+  );
+};
